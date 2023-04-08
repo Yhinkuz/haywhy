25fae83aff6e4f3fe8d09b29ad3652cac42a498e
Yhinkuz <olayinkadhulikifili@gmail.com>
2023-04-08 05:46:58 -0700

    first commit

---
+++ youtube.js
@@ -0,0 +1,141 @@
+// console.log(2 + 2);
+// console.log(1 + 2 + 3 + 4 + 5);
+// console.log(20 - 18);
+// console.log(2 * 3);
+// console.log(8 / 1);
+// console.log(3 > 2);
+// console.log(2 > 3);
+// console.log(10==9);
+// 2 + 2?
+
+// var result = 50;
+// if(result < 40) {
+//     console.log('You passed the text')
+// } else {
+//     console.log('you did not pass the test')
+// }
+
+// var place = 'first';
+// if(place == 'first') {
+//     console.log('Gold')
+// } else if (place == 'second') {
+//     console.log('Sliver') 
+// } else if (place == 'third') {
+//     console.log('Bronze')
+// } else {
+//     console.log('No medal')
+// }
+
+// var light = 'White'
+// if(light == 'green') {
+//     console.log('Drive')
+// } else if (light == 'White') {
+//     console.log ('Get ready')
+// } else if (light == 'red') {
+//     console.log("don't drive")
+// } else {
+//     console.log("The car is not green, orange, or red")
+// }
+
+// var light = 'green'
+// switch(light) {
+//     case 'green':
+//         console.log("Drive");
+//         break;
+//     case 'orange':
+//         console.log("Get ready");
+//         break;
+//     case 'red': 
+//         console.log("Don't drive");
+//         break;
+//     default:
+//         console.log('The ligh is not green, orange, or red')
+//         break;
+// }
+
+// var age = 10;
+// if(age >= 65) {
+//     console.log('You get your income from your pension')
+// } else if (age < 65 && age >= 18) {
+//     console.log('Each month you get a salary')
+// } else if (age < 18) {
+//     console.log('You get an allowance')
+// } else {
+//     console.log('The value of the age variable is not numerical')
+// }
+
+// for (var i = 1; i <= 3; i++) {
+//     console.log(i)
+// }
+// console.log('Go')
+
+// for (var i = 10; i > 0; i--) {
+//     console.log(i)
+// }
+// console.log('Happy New Month')
+
+// var counter = 3
+// while (counter > 0) {
+//     console.log(counter);
+//     counter = counter - 1;
+// }
+
+// for (var i = 1; i <= 5; i++) {
+//     console.log(i)
+// }
+//     console.log('Counting completed!')
+
+// for (var i = 5; i > 1; i--) {
+//     console.log(i);
+// };
+//     console.log('Countdown finished!')
+
+// var i = 1
+// while (i > 0) {
+//     console.log(counter);
+//     counter = counter - 1;
+// }
+
+// for (var i = 1; i <= 2; i++) {
+//     for (var j = 1; j <= 5; j++) {
+//         console.log("week" + i + "day" + j)
+//     }
+// }
+
+// for (var year = 2023; year < 2025; year++) {
+//     console.log(year)
+// }
+// for (var year = 2023; year < 2025; year++) {
+//     console.log(year);
+//     for (var month = 6; month < 9; month++) {
+//         console.log(".........", month)
+//     }
+// }
+
+// function addTwoNums(a,b) {
+//     var a = 10;
+//     var b = 20;
+//     var c = a + b;
+//     console.log(c);
+// }
+
+// addTwoNums()
+
+// function addTwoNums(a,b) {
+//     var c = a + b;
+//     console.log(c);
+// }
+// addTwoNums(2,2);
+// addTwoNums(4,4);
+
+var train1 = ["wheat", "barley", "potato", "salt", "rocks"];
+console.log(train1[2])
+
+// function listArrayItems(arr) {
+//     for (var i = 0; i < arr.lenght; i++) {
+//         console.log(arr[i])
+//     }
+// }
+// var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink'];
+// listArrayItems(colors); 
+
